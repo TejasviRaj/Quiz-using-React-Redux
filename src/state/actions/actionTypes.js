8bfbaa6ebbe281d3a4581eba4d95986212124da2
Tejasvi Raj Pant <tejpant@gmail.com>
2019-11-07 19:30:24 +0545

    Set up action types

---
+++ src/state/actions/actionTypes.js
@@ -0,0 +1,14 @@
+ // Author- Tejasvi Raj Pant
+
+const Actions = {
+    GOTO_NEXT_QUESTION: 'GOTO_NEXT_QUESTION',
+    GOTO_PREV_QUESTION: 'GOTO_PREV_QUESTION',
+    SELECT_ANSWER: 'SELECT_ANSWER',
+    DATA_LOADED: 'DATA_LOADED'
+
+}
+
+export default Actions;
+
+
+
