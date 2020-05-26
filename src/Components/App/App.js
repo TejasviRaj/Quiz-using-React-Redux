3babe24883e32bcfd613241193754a59503ccc58
Tejasvi Raj Pant <tejpant@gmail.com>
2020-05-26 22:45:51 +0545

    Created new component for title

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,18 +7,13 @@ import { Route } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
 import classes from './App.css';
 import Home from '../Home/Home'
+import Title from '../Title/Title'
 
 class App extends Component {
     render() {
         return (
             <React.Fragment >
-                  <div style={{ textAlign: "center", }} >
-                            <h2 class="section-heading"
-                                style={{ textTransform: "uppercase" }} > Quiz </h2>
-                            <h3 class="section-subheading"
-                                style={{ color: "#6c757d" }} > TEST YOUR KNOWLEDGE
-                                </h3>
-                        </div >
+                <Title></Title>
                 <section >
                     <div class={classes.quizContainer} >
                       
---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Title = (props) => {
+    return (<div style={{ textAlign: "center", }} >
+        <h2 class="section-heading"
+            style={{ textTransform: "uppercase" }} > Quiz </h2>
+        <h3 class="section-subheading"
+            style={{ color: "#6c757d" }} > TEST YOUR KNOWLEDGE
+        </h3>
+    </div >)
+};
+
+export default Title;
